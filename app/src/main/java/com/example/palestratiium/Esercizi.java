235f287a923deb12a3a00410770f39ab1309cabf
mpastorino1 <37106921+mpastorino1@users.noreply.github.com>
2022-01-09 21:29:15 +0100

    Qualche aggiustamennto
    stelle, back button

--- app/src/main/java/com/example/palestratiium/Esercizi.java
+++ app/src/main/java/com/example/palestratiium/Esercizi.java
@@ -4,17 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.io.Serializable;
 
 import classi.User;
 
 public class Esercizi extends AppCompatActivity {
+
     User user;
+    ImageView back;
+
+    public static final String EXTRA_USER = "package com.example.BonusLogin";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esercizi);
+
+        back = findViewById(R.id.back_button);
 
         Intent intent = getIntent();
         Serializable obj = intent.getSerializableExtra(Login.EXTRA_USER);
@@ -24,6 +34,15 @@ public class Esercizi extends AppCompatActivity {
         }else{
             user = new User();
         }
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent home = new Intent(Esercizi.this, Home.class);
+                home.putExtra(EXTRA_USER, user);
+                startActivity(home);
+            }
+        });
 
     }
 }
